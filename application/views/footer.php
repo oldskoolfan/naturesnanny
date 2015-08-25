18ab504e1e265cc4a698105e32b779e1d4164795
Andrew Harris <harris.1305@gmail.com>
2015-08-25 00:59:20 -0400

    livereload

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,9 +2,9 @@
 	<footer>
 	Copyright &copy; <?=date('Y')?> Nature's Nanny
 	</footer>
-	<script>
+	<!--script>
 		document.write('<script src="' + location.origin +
 		':35729/livereload.js?snipver=1" type="text/javascript"><\/script>');
-	</script>
+	</script-->
 </body>
 </html>
